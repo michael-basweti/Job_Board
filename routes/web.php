3d4a06c2dca07e1b1578c7a1eff66b369d7c1e06
Basweti <baswetima@gmail.com>
2019-06-17 10:28:03 +0300

    add tymon jwt

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,9 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+$router->group(['prefix'=>'api/v1/'], function () use ($router){
+
+    // $router->post('users', ['uses' => 'UserController@create']);
+    $router->post('login',['uses' => 'AuthController@postLogin']);
+
+});
