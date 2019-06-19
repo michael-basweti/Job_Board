80db787e778a69e98b51259fefe52ce5e7f4c28b
Basweti <baswetima@gmail.com>
2019-06-19 16:14:14 +0300

    validate user role

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -26,7 +26,7 @@ class UserController extends Controller
             'name' => 'required|string|max:255',
             'email'=>'required|string|email|max:255|unique:users',
             'password'=>'required|string|min:6',
-            'role'=>'required',
+            'role'=>'required|in:employer,applicant',
         ]);
 
 
