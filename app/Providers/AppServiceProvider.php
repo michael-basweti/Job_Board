2980b18e9e135c8a3e9e3c123a280913f9fac20c
Basweti <baswetima@gmail.com>
2019-06-26 15:36:31 +0300

    add https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,10 +15,9 @@ class AppServiceProvider extends ServiceProvider
     {
         $this->app->register(\Tymon\JWTAuth\Providers\LumenServiceProvider::class);
     }
-//     public function boot()
-// {
-//      if (!\App::environment('local')) {
-//           \URL::forceSchema('https');
-//      }
-// }
+    public function boot()
+    {
+        URL::forceScheme('https');
+    }
+
 }
